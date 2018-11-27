07eb5341cd29518ebeb9d85a37ad06cbdfab503a
Peter Por <petervpor@gmail.com>
2018-11-27 06:34:40 -0800

    S4L58

--- src/App.js
+++ src/App.js
@@ -5,26 +5,33 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: '1', name: 'Max', age: 28 },
+      { id: '2', name: 'Manu', age: 29 },
+      { id: '3', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-          { name: 'Max', age: 28 },
-          { name: event.target.value, age: 29 },
-          { name: 'Stephanie', age: 27 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+       return p.id === id;
+    });
+
+    const person = {
+        ...this.state.persons[personIndex] // copy personIndex into a new array
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   };
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    const persons = [...this.state.persons]; // spread operator to take elements of an object, expands it, and put it into a new array(ES6)
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
   };
@@ -51,7 +58,9 @@ class App extends Component {
                     return <Person
                         click={() => this.deletePersonHandler(index)}
                         name={person.name}
-                        age={person.age} />
+                        age={person.age}
+                        key={person.id}
+                        changed={(event) => this.nameChangedHandler(event, person.id)}/>
                 })}
             </div>
         );
